c3d68621320e78f54394a037c29e3ad7e11c4eba
Skenjaev <macbook@macbookui-MacBookPro.local>
2025-06-05 18:08:02 +0900

    feat: train callback hamda asynchronous functions

--- server.js
+++ server.js
@@ -1,30 +1,133 @@
-console.log("web Serverni boshlash");
+/*  Express.js Frameworki
 
+
+
+console.log("web Serverni boshlash");
+ 
 const express = require("express");
 const app = express();  // Express kutubxonasini chaqirish ya'ni APP belgisini quysay bizga expressni APP objecktini chaqirib beradi.
-const http = require("http");
+ const http = require("http");
 
-//express web servesi 4-ga bulinadi
+ //express web servesi 4-ga bulinadi
 
-// 1)Kirish code
+ // 1)Kirish code
 
-app.use(express.static("public")); //har qandfay brauzerdan kirib kelgan zaproslar uchun public folderi ochiq degan ma'noni anglatadi.
+ app.use(express.static("public")); //har qandfay brauzerdan kirib kelgan zaproslar uchun public folderi ochiq degan ma'noni anglatadi.
+ app.use(express.json());
+ app.use(express.urlencoded({ extended: true }));
+
+ // 2: Session code
+
+ // 3: Views code
+ app.set("views", "views");
+ app.set("view engine", "ejs");
+
+ // 4: Routing code  //Routing - bu serverga kelgan so'rovlarni qaysi yo'lga yuborishni belgilaydi. Masalan, GET, POST, PUT, DELETE so'rovlarini qayta ishlash uchun ishlatiladi.
+ app.get("/hello", function (req, res) {
+     res.end("<h1>HELLO WORLD</h1>");
+ });
+
+ app.get("/gift", function (req, res) {
+     res.end("<h1>Siz sovgalar bolimidasiz</h1>");
+ });
+
+ const server = http.createServer(app);
+ let PORT = 3000;
+
+ server.listen(PORT, function () {
+     console.log(`The server is running successfully on port: ${PORT}`);
+});
+*/
+
+
+
+
+// EJS Frameworki
+
+
+
+ /*
+ console.log("web Serverni boshlash");
+
+ const express = require("express");
+ const app = express();  // Express kutubxonasini chaqirish ya'ni APP belgisini quysay bizga expressni APP objecktini chaqirib beradi.
+ const http = require("http");
+
+ //express web servesi 4-ga bulinadi
+
+ // 1)Kirish code
+
+app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// 2: Session code
-
-// 3: Views code
+// 2:Session code
+// 3:Views code
 app.set("views", "views");
 app.set("view engine", "ejs");
 
-// 4: Routing code  //Routing - bu serverga kelgan so'rovlarni qaysi yo'lga yuborishni belgilaydi. Masalan, GET, POST, PUT, DELETE so'rovlarini qayta ishlash uchun ishlatiladi.
-app.get("/hello", function (req, res) {
-    res.end("<h1>HELLO WORLD</h1>");
+
+// 4:Routing code
+app.post("/create-item", (req, res) => {
+    // TODO: code with db here
 });
 
-app.get("/gift", function (req, res) {
-    res.end("<h1>Siz sovgalar bolimidasiz</h1>");
+app.get("/", function (req, res) {
+    res.render("index");
+});
+
+const server = http.createServer(app);
+let PORT = 3000;
+
+server.listen(PORT, function () {
+    console.log(`The server is running successfully on port: ${PORT}`);
+});
+*/
+
+
+
+
+//EJS Frameworkda Portfolia publishing qilamiz.
+
+
+console.log("web Serverni boshlash");
+
+const express = require("express");
+const app = express();
+const http = require("http");
+const fs = require("fs");
+
+// express web servesi 4-ga bulinadi
+
+// 1)Kirish code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// 2:Session code
+// 3:Views code
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// 4:Routing code
+app.post("/create-item", (req, res) => {
+    // TODO: code with db here
+});
+
+app.get('/author', (req, res) => {
+    fs.readFile("database/user.json", "utf-8", (err, data) => {
+        if(err) {
+            console.log("ERROR:", err);
+            res.status(500).send("Xatolik yuz berdi");
+        } else {
+            const user = JSON.parse(data);
+            res.render("author", { user: user });
+        }
+    });
+}); 
+
+app.get("/", function (req, res) {
+    res.render("harid"); 
 });
 
 const server = http.createServer(app);
--- train.js
+++ train.js
@@ -1,1 +1,200 @@
-console.log('train task ishga tushdi!')
+//NodeJS event loop, Callback, Asynchronous functionlar & MITASK-A;
+
+// console.log("Jack Ma maslahatlari");
+
+// const list = [
+//   "yahshi talaba bo'ling", // 0-20
+//   "to'g'ri boshliq tanlang va ko'proq xato qiling", // 20-30
+//   "o'zingizga ishlashingizni boshlang", // 30-40
+//   "siz kuchli bo'lgan narsalarni qiling", // 40-50
+//   "yoshlarga investitsiya qiling", // 50-60
+//   "endi dam oling, foydasi yo'q endi" // 60+
+// ];
+
+// function maslahatBering(a, callback) {
+//   if (typeof a !== "number") {
+//     callback("Insert a number", null);
+//   } else if (a <= 20) {
+//     callback(null, list[0]);
+//   } else if (a > 20 && a < 30) {
+//     callback(null, list[1]);
+//   } else if (a >= 30 && a <= 40) {
+//     callback(null, list[2]);
+//   } else if (a > 40 && a < 50) {
+//     callback(null, list[3]);
+//   } else if (a >= 50 && a <= 60) {
+//     callback(null, list[4]);
+//   } else {
+//     setTimeout(function () {
+//       callback(null, list[5]);
+//     }, 5000);
+//   }
+// }
+
+// console.log("Passed here 0");
+
+// maslahatBering(65, (err, data) => {
+//   if (err) {
+//     console.log("ERROR:", err);
+//   } else {
+//     console.log("Javob:", data);
+//   }
+// });
+
+// console.log("Passed here 1");
+
+
+
+
+
+// NodeJS event loop, Callback, Asynchronous functionlar & MITASK-A 2-qismi.
+
+
+
+
+
+
+
+console.log("Jack Ma maslahatlari");
+
+const list = [
+  "yahshi talaba bo'ling", // 0-20
+  "to'g'ri boshliq tanlang va ko'proq xato qiling", // 20-30
+  "o'zingizga ishlashingizni boshlang", // 30-40
+  "siz kuchli bo'lgan narsalarni qiling", // 40-50
+  "yoshlarga investitsiya qiling", // 50-60
+  "endi dam oling, foydasi yo'q endi" // 60+
+];
+
+
+// CALLBACK FUNCTION
+
+function maslahatBering(a, callback) {
+    if (typeof a !== "number") {
+        callback("Insert a number", null);
+    }
+    else if (a <= 20) {
+        callback(null, list[0]);
+    }
+    else if (a > 20 && a <= 30) {
+        callback(null, list[1]);
+    }
+    else if (a > 30 && a <= 40) {
+        callback(null, list[2]);
+    }
+    else if (a > 40 && a <= 50) {
+        callback(null, list[3]);
+    }
+    else if (a > 50 && a <= 60) {
+        callback(null, list[4]);
+    }
+    else {
+        setTimeout(function() {
+            callback(null, list[5]);
+        }, 1000);
+    }
+}
+
+console.log("passed here 0");
+
+maslahatBering(75, (err, data) => {
+    if (err) {
+        console.log("ERROR:", err);
+    } else {
+        console.log(data);
+    }
+});
+
+console.log("passed here 1");
+
+
+
+// ASYNCHRONOUS FUNCTION
+
+
+async function maslahatBering(a) {
+  if (typeof a !== "number") {
+    throw new Error("Insert a number");
+  } else if (a <= 20) {
+    return list[0];
+  } else if (a > 20 && a <= 30) {
+    return list[1];
+  } else if (a > 30 && a <= 40) {
+    return list[2];
+  } else if (a > 40 && a <= 50) {
+    return list[3];
+  } else if (a > 50 && a <= 60) {
+    return list[4];
+  } else {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(list[5]);
+      }, 5000);
+    });
+  }
+}
+
+
+
+
+// CALL VIA THEN/CATCH
+
+
+
+// then/catch
+// console.log("passed here 0");
+// maslahatBering(65)
+//   .then((data) => {
+//     console.log("javob:", data);
+//   })
+//   .catch((err) => {
+//     console.log("ERROR:", err);
+//   });
+// console.log("passed here 1");
+
+
+// CALL VIA ASYNC/AWAIT
+
+
+// async/await
+// async function run() {
+//     try {
+//       let javob = await maslahatBering(25);
+//       console.log(javob);
+      
+//       javob = await maslahatBering(35);
+//       console.log(javob);
+      
+//       javob = await maslahatBering(41);
+//       console.log(javob);
+//     } catch (error) {
+//       console.log("ERROR:", error);
+//     }
+//   }
+  
+//   run();
+
+
+
+
+
+//  TASK A 
+
+
+
+// function countLetter(harf, soz) {
+//     let soni = 0;
+//     for (let belgi of soz) {
+//       if (belgi === harf) {
+//         soni++;
+//       }
+//     }
+//     return soni;
+//   }
+  
+//   const harf = "a";
+//   const soz = "banana";
+//   console.log(
+//     `${soz} so'zida '${harf}' harfi ${countLetter(harf, soz)} marta takrorlangan`
+//   );
+
