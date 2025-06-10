437e4eeb004e4b12003449d60dac083fb52ba849
Skenjaev <macbook@macbookui-MacBookPro.local>
2025-06-10 21:22:43 +0900

    feat: mongoDB ulanish logik

--- train.js
+++ train.js
@@ -198,3 +198,41 @@ async function maslahatBering(a) {
 //     `${soz} so'zida '${harf}' harfi ${countLetter(harf, soz)} marta takrorlangan`
 //   );
 
+
+
+
+//         B-TASK: B-TASK: 
+
+/*Shunday function tuzing, u 1ta string parametrga ega bolsin, hamda osha stringda qatnashgan raqamlarni sonini bizga return qilsin.
+MASALAN countDigits("ad2a54y79wet0sfgb9") 7ni return qiladi.*/
+
+
+
+// My Solution - Big O(n)
+const countDigits = (str) => {
+  return str.split('').map(Number).filter(char => !isNaN(char)).length
+}
+console.log(countDigits(("ad2a54y79wet0sfgb9"))); //7
+
+// Alternative solution 1: Using reduce - Big O(n)
+const countDigitsReduce = (str) => {
+  return str.split('').reduce((count, char) => !isNaN(Number(char)) ? count + 1 : count, 0);
+}
+console.log(countDigitsReduce("ad2a54y79wet0sfgb9")); //7
+
+// Alternative solution 2: Using regex - Big O(n)
+const countDigitsRegex = (str) => {
+  const regex = /\d/g; // \d matches any digit
+  return (str.match(regex) || []).length;
+}
+console.log(countDigitsRegex("ad2a54y79wet0sfgb9")); //7
+
+// Alternative solution 3: Using for...of loop - Big O(n)
+const countDigitsLoop = (str) => {
+  let count = 0;
+  for (let char of str) {
+      if (!isNaN(Number(char))) count++;
+  }
+  return count;
+}
+console.log(countDigitsLoop("ad2a54y79wet0sfgb9")); 
