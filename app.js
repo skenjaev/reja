fee842e22efd71fc26376fcf98c89f69c820f770
Skenjaev <macbook@macbookui-MacBookPro.local>
2025-06-10 10:01:47 +0900

    feat: mongoDB ulanish logik

--- app.js
+++ app.js
@@ -19,12 +19,32 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4: Routing code
+
 app.post("/create-item", (req, res) => {
-    // TODO: code with db here
+    console.log("user entered /create-item");
+    const new_reja = req.body.reja;
+    
+    db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end("something went wrong");
+        } else {
+            res.end("successfully added");
+        }
+    });
 });
 
-app.get("/", function(req, res) {
-    res.render("reja");
+app.get("/", function (req, res) {
+    console.log("user entered /");
+    
+    db.collection("plans").find().toArray((err, data) => {
+        if (err) {
+            console.log(err);
+            res.end("something went wrong");
+        } else {
+            res.render("reja", { items: data });
+        }
+    });
 });
 
 module.exports = app;
