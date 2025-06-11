b205182a88517240c132623e810aad267ae10801
Skenjaev <macbook@macbookui-MacBookPro.local>
2025-06-12 00:20:08 +0900

    feat: [26-27] Reja mini loyihamizning frontendini quramiz va Rejalarni o'chirish (DELETE) & MITASK-C

--- commit.sh
+++ commit.sh
@@ -1,3 +1,3 @@
 git add .
-git commit -m "feat: mongoDB ulanish logik"
+git commit -m "feat: [26-27] Reja mini loyihamizning frontendini quramiz va Rejalarni o'chirish (DELETE) & MITASK-C"
 git push origin master
